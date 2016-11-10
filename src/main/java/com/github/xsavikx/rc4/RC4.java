11fb3c5e7cc235e47ad55465370f1af2e95f2e54
Iurii Sergiichuk <iurii.sergiichuk@gmail.com>
2016-11-10 21:18:18 +0200

    updated implementation improved unit tests updated pom.xml

--- src/main/java/com/github/xsavikx/rc4/RC4.java
+++ src/main/java/com/github/xsavikx/rc4/RC4.java
@@ -4,185 +4,167 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
+/**
+ * Implementation of RC4 stream cipher
+ *
+ * @author Iurii Sergiichuk
+ */
 public class RC4 {
-	/**
-	 * Key array
-	 */
-	private byte[] key = new byte[SBOX_LENGTH - 1];
-	/**
-	 * Sbox
-	 */
-	private int[] sbox = new int[SBOX_LENGTH];
-	private static final int SBOX_LENGTH = 256;
-	private static final int KEY_MIN_LENGTH = 5;
+    private static final int SBOX_LENGTH = 256;
+    private static final int KEY_MIN_LENGTH = 5;
+    /**
+     * Key array
+     */
+    private byte[] key = new byte[SBOX_LENGTH - 1];
+    /**
+     * Sbox
+     */
+    private int[] sbox = new int[SBOX_LENGTH];
 
-	public RC4() {
-		reset();
-	}
+    public RC4() {
+        reset();
+    }
 
-	public RC4(String key) throws InvalidKeyException {
-		this();
-		setKey(key);
-	}
+    public RC4(String key) throws InvalidKeyException {
+        this();
+        setKey(key);
+    }
 
-	private void reset() {
-		for (int i = 0; i < key.length; i++)
-			key[i] = 0;
-		Arrays.fill(sbox, 0);
-	}
+    private void reset() {
+        Arrays.fill(key, (byte) 0);
+        Arrays.fill(sbox, 0);
+    }
 
-	/**
-	 * Encrypt given message String with given Charset and key
-	 *
-	 * @param message
-	 *            message to be encrypted
-	 * @param charset
-	 *            charset of message
-	 * @param key
-	 *            key
-	 * @return encrypted message
-	 * @throws InvalidKeyException
-	 *             if key length is smaller than 5 or bigger than 255
-	 */
-	public byte[] encryptMessage(String message, Charset charset, String key)
-			throws InvalidKeyException {
-		reset();
-		setKey(key);
-		byte[] crypt = crypt(message.getBytes());
-		reset();
-		return crypt;
-	}
+    /**
+     * Encrypt given message String with given Charset and key
+     *
+     * @param message message to be encrypted
+     * @param charset charset of message
+     * @param key     key
+     * @return encrypted message
+     * @throws InvalidKeyException if key length is smaller than 5 or bigger than 255
+     */
+    public byte[] encryptMessage(String message, Charset charset, String key)
+            throws InvalidKeyException {
+        reset();
+        setKey(key);
+        byte[] crypt = crypt(message.getBytes());
+        reset();
+        return crypt;
+    }
 
-	/**
-	 * Encrypt given message String with given Key and pre-defined UTF-8 charset
-	 *
-	 * @param message
-	 *            message to be encrypted
-	 * @param key
-	 *            key
-	 * @return encrypted message
-	 * @throws InvalidKeyException
-	 *             if key length is smaller than 5 or bigger than 255
-	 * @see StandardCharsets
-	 */
-	public byte[] encryptMessage(String message, String key)
-			throws InvalidKeyException {
-		return encryptMessage(message, StandardCharsets.UTF_8, key);
-	}
+    /**
+     * Encrypt given message String with given Key and pre-defined UTF-8 charset
+     *
+     * @param message message to be encrypted
+     * @param key     key
+     * @return encrypted message
+     * @throws InvalidKeyException if key length is smaller than 5 or bigger than 255
+     * @see StandardCharsets
+     */
+    public byte[] encryptMessage(String message, String key)
+            throws InvalidKeyException {
+        return encryptMessage(message, StandardCharsets.UTF_8, key);
+    }
 
-	/**
-	 * Decrypt given byte[] message array with given charset and key
-	 *
-	 * @param message
-	 *            message to be decrypted
-	 * @param charset
-	 *            charset of message
-	 * @param key
-	 *            key
-	 * @return string in given charset
-	 * @throws InvalidKeyException
-	 *             if key length is smaller than 5 or bigger than 255
-	 */
-	public String decryptMessage(byte[] message, Charset charset, String key)
-			throws InvalidKeyException {
-		reset();
-		setKey(key);
-		byte[] msg = crypt(message);
-		reset();
-		return new String(msg);
-	}
+    /**
+     * Decrypt given byte[] message array with given charset and key
+     *
+     * @param message message to be decrypted
+     * @param charset charset of message
+     * @param key     key
+     * @return string in given charset
+     * @throws InvalidKeyException if key length is smaller than 5 or bigger than 255
+     */
+    public String decryptMessage(byte[] message, Charset charset, String key)
+            throws InvalidKeyException {
+        reset();
+        setKey(key);
+        byte[] msg = crypt(message);
+        reset();
+        return new String(msg);
+    }
 
-	/**
-	 * Decrypt given byte[] message array with given key and pre-defined UTF-8
-	 * charset
-	 *
-	 * @param message
-	 *            message to be decrypted
-	 * @param key
-	 *            key
-	 * @return string in given charset
-	 * @throws InvalidKeyException
-	 *             if key length is smaller than 5 or bigger than 255
-	 * @see StandardCharsets
-	 */
-	public String decryptMessage(byte[] message, String key)
-			throws InvalidKeyException {
-		return decryptMessage(message, StandardCharsets.UTF_8, key);
-	}
+    /**
+     * Decrypt given byte[] message array with given key and pre-defined UTF-8
+     * charset
+     *
+     * @param message message to be decrypted
+     * @param key     key
+     * @return string in given charset
+     * @throws InvalidKeyException if key length is smaller than 5 or bigger than 255
+     * @see StandardCharsets
+     */
+    public String decryptMessage(byte[] message, String key)
+            throws InvalidKeyException {
+        return decryptMessage(message, StandardCharsets.UTF_8, key);
+    }
 
-	/**
-	 * Crypt given byte array. Be aware, that you must init key, before using
-	 * crypt.
-	 *
-	 * @param msg
-	 *            array to be crypt
-	 * @return crypted byte array
-	 * @see <a
-	 *      href="http://en.wikipedia.org/wiki/RC4#Pseudo-random_generation_algorithm_.28PRGA.29">Pseudo-random
-	 *      generation algorithm</a>
-	 */
-	public byte[] crypt(final byte[] msg) {
-		sbox = initSBox(key);
-		byte[] code = new byte[msg.length];
-		int i = 0;
-		int j = 0;
-		for (int n = 0; n < msg.length; n++) {
-			i = (i + 1) % SBOX_LENGTH;
-			j = (j + sbox[i]) % SBOX_LENGTH;
-			swap(i, j, sbox);
-			int rand = sbox[(sbox[i] + sbox[j]) % SBOX_LENGTH];
-			code[n] = (byte) (rand ^ msg[n]);
-		}
-		return code;
-	}
+    /**
+     * Crypt given byte array. Be aware, that you must init key, before using
+     * crypt.
+     *
+     * @param msg array to be crypt
+     * @return crypted byte array
+     * @see <a href="http://en.wikipedia.org/wiki/RC4#Pseudo-random_generation_algorithm_.28PRGA.29">Pseudo-random generation algorithm</a>
+     */
+    public byte[] crypt(final byte[] msg) {
+        sbox = initSBox(key);
+        byte[] code = new byte[msg.length];
+        int i = 0;
+        int j = 0;
+        for (int n = 0; n < msg.length; n++) {
+            i = (i + 1) % SBOX_LENGTH;
+            j = (j + sbox[i]) % SBOX_LENGTH;
+            swap(i, j, sbox);
+            int rand = sbox[(sbox[i] + sbox[j]) % SBOX_LENGTH];
+            code[n] = (byte) (rand ^ msg[n]);
+        }
+        return code;
+    }
 
-	/**
-	 * Initialize SBOX with given key. Key-scheduling algorithm
-	 *
-	 * @param key
-	 *            key
-	 * @return sbox int array
-	 * @see <a
-	 *      href="http://en.wikipedia.org/wiki/RC4#Key-scheduling_algorithm_.28KSA.29">Wikipedia.
-	 *      Init sbox</a>
-	 */
-	private int[] initSBox(byte[] key) {
-		int[] sbox = new int[SBOX_LENGTH];
-		int j = 0;
+    /**
+     * Initialize SBOX with given key. Key-scheduling algorithm
+     *
+     * @param key key
+     * @return sbox int array
+     * @see <a href="http://en.wikipedia.org/wiki/RC4#Key-scheduling_algorithm_.28KSA.29">Wikipedia. Init sbox</a>
+     */
+    private int[] initSBox(byte[] key) {
+        int[] sbox = new int[SBOX_LENGTH];
+        int j = 0;
 
-		for (int i = 0; i < SBOX_LENGTH; i++) {
-			sbox[i] = i;
-		}
+        for (int i = 0; i < SBOX_LENGTH; i++) {
+            sbox[i] = i;
+        }
 
-		for (int i = 0; i < SBOX_LENGTH; i++) {
-			j = (j + sbox[i] + (0xFF & (int)key[i % key.length])) % SBOX_LENGTH;
-			swap(i, j, sbox);
-		}
-		return sbox;
-	}
+        for (int i = 0; i < SBOX_LENGTH; i++) {
+            j = (j + sbox[i] + (key[i % key.length]) & 0xFF) % SBOX_LENGTH;
+            swap(i, j, sbox);
+        }
+        return sbox;
+    }
 
-	private void swap(int i, int j, int[] sbox) {
-		int temp = sbox[i];
-		sbox[i] = sbox[j];
-		sbox[j] = temp;
-	}
+    private void swap(int i, int j, int[] sbox) {
+        int temp = sbox[i];
+        sbox[i] = sbox[j];
+        sbox[j] = temp;
+    }
 
-	/**
-	 * Setup key
-	 *
-	 * @param key
-	 *            key to be setup
-	 * @throws InvalidKeyException
-	 *             if key length is smaller than 5 or bigger than 255
-	 */
-	public void setKey(String key) throws InvalidKeyException {
-		if (!(key.length() >= KEY_MIN_LENGTH && key.length() < SBOX_LENGTH)) {
-			throw new InvalidKeyException("Key length has to be between "
-					+ KEY_MIN_LENGTH + " and " + (SBOX_LENGTH - 1));
-		}
+    /**
+     * Setup key
+     *
+     * @param key key to be setup
+     * @throws InvalidKeyException if key length is smaller than 5 or bigger than 255
+     */
+    public void setKey(String key) throws InvalidKeyException {
+        if (!(key.length() >= KEY_MIN_LENGTH && key.length() < SBOX_LENGTH)) {
+            throw new InvalidKeyException("Key length has to be between "
+                    + KEY_MIN_LENGTH + " and " + (SBOX_LENGTH - 1));
+        }
 
-		this.key = key.getBytes();
-	}
+        this.key = key.getBytes();
+    }
 
 }
 
@@ -191,12 +173,12 @@ public class RC4 {
  *
  * @author Iurii Sergiichuk
  */
-class InvalidKeyException extends Exception {
+class InvalidKeyException extends RuntimeException {
 
-	private static final long serialVersionUID = -2412232436238451574L;
+    private static final long serialVersionUID = -2412232436238451574L;
 
-	public InvalidKeyException(String message) {
-		super(message);
-	}
+    public InvalidKeyException(String message) {
+        super(message);
+    }
 
 }
--- src/test/java/com/github/xsavikx/rc4/RC4Test.java
+++ src/test/java/com/github/xsavikx/rc4/RC4Test.java
@@ -1,27 +1,44 @@
 package com.github.xsavikx.rc4;
 
-import static org.junit.Assert.*;
-
-import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class RC4Test {
-	private RC4 rc4;
-	private String message;
-	private String key;
 
-	@Before
-	public void setUp() throws Exception {
-		rc4 = new RC4();
-		message = "Hello, World!";
-		key = "This is pretty long key";
-	}
+    @Test
+    public void testCryptMessage() {
+        RC4 rc4 = new RC4();
+        String message = "Hello, World!";
+        String key = "This is pretty long key";
+        byte[] crypt = rc4.encryptMessage(message, key);
+        String msg = rc4.decryptMessage(crypt, key);
+        assertEquals(message, msg);
+    }
 
-	@Test
-	public void testCryptMessage() throws InvalidKeyException {
-		byte[] crypt = rc4.encryptMessage(message, key);
-		String msg = rc4.decryptMessage(crypt, key);
-		assertEquals(message, msg);
-	}
+    @Test
+    public void testCryptWithNonEnglishCharacters() {
+        String message = "Привет, Мир!";
+        String key = "Это довольно длинный ключ";
+        RC4 rc4 = new RC4(key);
+        byte[] crypt = rc4.crypt(message.getBytes());
+        byte[] msg = rc4.crypt(crypt);
+        assertEquals(message, new String(msg));
+    }
+
+    @Test(expected = InvalidKeyException.class)
+    public void testSetKeyShouldThrowInvalidKeyExceptionWithTooSmallKey() {
+        new RC4("");
+    }
+
+    @Test(expected = InvalidKeyException.class)
+    public void testSetKeyShouldThrowInvalidKeyExceptionWithTooBigKey() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 512; i++) {
+            sb.append('a');
+        }
+        RC4 rc4 = new RC4();
+        rc4.setKey(sb.toString());
+    }
 
 }
